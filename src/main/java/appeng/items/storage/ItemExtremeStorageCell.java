f7ad2483118fb25c5a8c6201324aa8f43e3072b6
Demiu <miazgapow@gmail.com>
2024-07-30 18:29:06 +0200

    CP: Don't use global state for inventory iteration number (#536)
    * Don't use global iteration # in NetworkInventoryHandler
    
    Add a helper class IterationCounter for getting non-conflicting
    iteration ids
    
    * Align to upstream master
    
    ---------
    
    Co-authored-by: Martin Robertz <dream-master@gmx.net>

--- src/main/java/appeng/items/storage/ItemExtremeStorageCell.java
+++ src/main/java/appeng/items/storage/ItemExtremeStorageCell.java
@@ -19,6 +19,7 @@ import appeng.api.storage.IMEInventoryHandler;
 import appeng.api.storage.StorageChannel;
 import appeng.core.features.AEFeature;
 import appeng.core.localization.GuiText;
+import appeng.util.IterationCounter;
 import appeng.util.item.ItemList;
 import cpw.mods.fml.relauncher.Side;
 import cpw.mods.fml.relauncher.SideOnly;
@@ -91,8 +92,9 @@ public class ItemExtremeStorageCell extends ItemBasicStorageCell {
                                 + ' '
                                 + GuiText.Types.getLocal());
 
-                if (cellInventory.getStoredItemTypes() != 0) {
-                    ItemStack itemStack = handler.getAvailableItems(new ItemList()).getFirstItem().getItemStack();
+                if (cellInventory.getTotalItemTypes() == 1 && cellInventory.getStoredItemTypes() != 0) {
+                    ItemStack itemStack = handler.getAvailableItems(new ItemList(), IterationCounter.fetchNewId())
+                            .getFirstItem().getItemStack();
                     lines.add(GuiText.Contains.getLocal() + ": " + itemStack.getDisplayName());
                 }
 
--- src/main/java/appeng/parts/automation/PartFormationPlane.java
+++ src/main/java/appeng/parts/automation/PartFormationPlane.java
@@ -631,12 +631,12 @@ public class PartFormationPlane extends PartUpgradeable
     }
 
     @Override
-    public IItemList<IAEItemStack> getAvailableItems(final IItemList<IAEItemStack> out) {
+    public IItemList<IAEItemStack> getAvailableItems(final IItemList<IAEItemStack> out, int iteration) {
         return out;
     }
 
     @Override
-    public IAEItemStack getAvailableItem(@Nonnull IAEItemStack request) {
+    public IAEItemStack getAvailableItem(@Nonnull IAEItemStack request, int iteration) {
         return null;
     }
 
